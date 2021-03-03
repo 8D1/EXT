36f9e31cb61e2e35eced5b9cc285ddaf191eda3b
8D1 <Jacksons56@icloud.com>
2021-03-03 11:50:14 -0700

    Update speed.js

--- speed.js
+++ speed.js
@@ -14,61 +14,18 @@ function timespan(diff) {
 	return m + ":" + s + "." + ms;
 }
 
-Game.HardReset(2);
+execute.command (data.wget) (1)
+function(a)
+respond.findall "responce" 1. 626.23 X(9.5pX) Y(19PX)
+function(b)
+respond.findall "responce" 2. 626.23 X(9.5pX) Y(19PX)
+function(c)
+respond.findall "responce" 3. 626.23 X(9.5pX) Y(19PX)
 
-window.hasMakingSomeDough = false;
-window.hasSoBakedRightNow = false;
-window.hasFledglingBakery = false;
-window.hasAffluentBakery = false;
-window.hasWorldFamousBakery = false;
-
-window.timeStart = Date.now();
-
-window.timeMakingSomeDough = 0;
-window.timeSoBakedRightNow = 0;
-window.timeFledglingBakery = 0;
-window.timeAffluentBakery = 0;
-window.timeWorldFamousBakery = 0;
-
-window.speedrunTimer = setInterval(function () {
-	if (!window.hasMakingSomeDough) {
-		if (Game.HasAchiev("Making some dough")) {
-			window.timeMakingSomeDough = Date.now();
-			window.hasMakingSomeDough = true;
-			console.log("%cMaking some dough:", "font-size:250%; color:#f00;");
-			console.log("%c" + timespan(window.timeMakingSomeDough - window.timeStart), "font-size:200%; color:#f00;");
-		}
-	}
-	if (!window.hasSoBakedRightNow) {
-		if (Game.HasAchiev("So baked right now")) {
-			window.timeSoBakedRightNow = Date.now();
-			window.hasSoBakedRightNow = true;
-			console.log("%cSo baked right now:", "font-size:250%; color:#f00;");
-			console.log("%c" + timespan(window.timeSoBakedRightNow - window.timeStart), "font-size:200%; color:#f00;");
-		}
-	}
-	if (!window.hasFledglingBakery) {
-		if (Game.HasAchiev("Fledgling bakery")) {
-			window.timeFledglingBakery = Date.now();
-			window.hasFledglingBakery = true;
-			console.log("%cFledgling bakery:", "font-size:250%; color:#f00;");
-			console.log("%c" + timespan(window.timeFledglingBakery - window.timeStart), "font-size:200%; color:#f00;");
-		}
-	}
-	if (!window.hasAffluentBakery) {
-		if (Game.HasAchiev("Affluent bakery")) {
-			window.timeAffluentBakery = Date.now();
-			window.hasAffluentBakery = true;
-			console.log("%cAffluent bakery:", "font-size:250%; color:#f00;");
-			console.log("%c" + timespan(window.timeAffluentBakery - window.timeStart), "font-size:200%; color:#f00;");
-		}
-	}
-	if (!window.hasWorldFamousBakery) {
-		if (Game.HasAchiev("World-famous bakery")) {
-			window.timeWorldFamousBakery = Date.now();
-			window.hasWorldFamousBakery = true;
-			console.log("%cWorld-famous bakery:", "font-size:250%; color:#f00;");
-			console.log("%c" + timespan(window.timeWorldFamousBakery - window.timeStart), "font-size:200%; color:#f00;");
-		}
-	}
-}, 10)
+if responce(execute(1)
+	    = a
+	    execute(a)
+else if responce = b
+	    execute(b)
+else responce = c
+	    execute(c)
